5a9cb491ea505c3a8f9e261b8bc22c96bdc3feb4
Coral Cota Riera <ccota@intricom.es>
2023-10-02 13:06:49 +0200

    añadidos cambios

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,25 @@ const escribirTurno  = (document.getElementById("seleccionar-turno"))
 const seleccionarTurno  = (document.getElementById("seleccionar")) 
 
 function restarTurno () {
-   if (numeroPrincipal !== null && numeroPrincipal !== undefined && parseInt(numeroPrincipal.innerHTML) > 0)
+   if (numeroPrincipal !== null && numeroPrincipal !== undefined  && numeroPrincipal instanceof HTMLHeadingElement && parseInt(numeroPrincipal.innerHTML) > 0)
    {
       numeroPrincipal.innerHTML = (parseInt(numeroPrincipal.innerHTML) -1 ).toString().padStart(2,"0")  ;
    }
 }
 function reset () {
-   if (numeroPrincipal !== null && numeroPrincipal !== undefined){
+   if (numeroPrincipal !== null && numeroPrincipal !== undefined && numeroPrincipal instanceof HTMLHeadingElement){
       numeroPrincipal.innerHTML = "00" ;
    }
 }
 function sumarTurno () {
-   if (numeroPrincipal !== null && numeroPrincipal !== undefined){
+   if (numeroPrincipal !== null && numeroPrincipal !== undefined && numeroPrincipal instanceof HTMLHeadingElement){
       numeroPrincipal.innerHTML = (parseInt(numeroPrincipal.innerHTML) + 1).toString().padStart(2,"0")   ;
    }
 }
 function seleccionar () {
    if (numeroPrincipal !== null && 
-      numeroPrincipal !== undefined && 
+      numeroPrincipal !== undefined  && 
+      numeroPrincipal instanceof HTMLHeadingElement && 
       escribirTurno !== null && 
       escribirTurno !== undefined && 
       escribirTurno instanceof HTMLInputElement) 
@@ -34,18 +35,22 @@ function seleccionar () {
 }
 
 if (botonAnterior !== null && 
-botonAnterior !== undefined){
+botonAnterior !== undefined && 
+botonAnterior instanceof HTMLButtonElement){
    botonAnterior.addEventListener("click", restarTurno) 
 }
 if (botonReset !== null && 
-botonReset !== undefined) {
+botonReset !== undefined && 
+botonReset instanceof HTMLButtonElement) {
    botonReset.addEventListener("click", reset) 
 }
 if(botonSiguiente !== null && 
-botonSiguiente !== undefined) {
+botonSiguiente !== undefined && 
+botonSiguiente instanceof HTMLButtonElement) {
 botonSiguiente.addEventListener("click", sumarTurno) 
 }
 if( seleccionarTurno !== null && 
-   seleccionarTurno !== undefined ){
+   seleccionarTurno !== undefined && 
+   seleccionarTurno instanceof HTMLButtonElement){
       seleccionarTurno.addEventListener("click", seleccionar)
    }
